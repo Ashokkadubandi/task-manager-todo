724ff1d274544afc30c81b521f5e03b46ed7895b
AshokKadubandi <velmaashok765@gmail.com>
2025-06-18 18:31:32 +0530

    configured cors policies

--- Fullstack/Backend/server.js
+++ Fullstack/Backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const {connectedDB} = require('./utils/db')
 const taskRoute = require('./routes/taskRoutes.js')
+const CORS = require('cors')
 const app = express()
 require('dotenv').config()
 const {PORT} = process.env
 
 app.use(express.json())
+app.use(CORS())
 
 const initilizeDBServer = async () =>{
     app.listen(PORT,() => {
